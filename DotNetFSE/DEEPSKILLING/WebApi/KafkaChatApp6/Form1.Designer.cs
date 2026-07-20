392928cbee0f7b62dc3244616e7797ce75ac9ef8
avulabhavana <avulabhavana226@gmail.com>
2026-07-20 23:05:46 +0530

    Add files via upload

---
+++ DotNetFSE/DEEPSKILLING/WebApi/KafkaChatApp6/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace KafkaChatApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Message = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lstMessages = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(58, 78);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(67, 16);
+            this.Message.TabIndex = 0;
+            this.Message.Text = " Message";
+            this.Message.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(35, 36);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(681, 22);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(234, 115);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(212, 34);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lstMessages
+            // 
+            this.lstMessages.FormattingEnabled = true;
+            this.lstMessages.ItemHeight = 16;
+            this.lstMessages.Location = new System.Drawing.Point(35, 196);
+            this.lstMessages.Name = "lstMessages";
+            this.lstMessages.Size = new System.Drawing.Size(681, 212);
+            this.lstMessages.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstMessages);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.Message);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lstMessages;
+    }
+}
+
---
+++ DotNetFSE/DEEPSKILLING/WebApi/KafkaChatApp6/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Confluent.Kafka;
+
+namespace KafkaChatApp6
+{
+    public partial class Form1 : Form
+    {
+        ProducerConfig producerConfig = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+
+        ConsumerConfig consumerConfig = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "chat-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartConsumer();
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMessage.Text))
+                return;
+
+            using (var producer = new ProducerBuilder<Null, string>(producerConfig).Build())
+            {
+                await producer.ProduceAsync("chatapp",
+                    new Message<Null, string>
+                    {
+                        Value = txtMessage.Text
+                    });
+
+                txtMessage.Clear();
+            }
+        }
+
+        private void StartConsumer()
+        {
+            Task.Run(() =>
+            {
+                using (var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build())
+                {
+                    consumer.Subscribe("chatapp");
+
+                    while (true)
+                    {
+                        var result = consumer.Consume();
+
+                        this.Invoke(new Action(() =>
+                        {
+                            lstMessages.Items.Add(result.Message.Value);
+                        }));
+                    }
+                }
+            });
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
